001cd14367a9c9853732ffada3492f04a7ae87d0
mariusposogan <mcposogan@gmail.com>
2024-04-05 15:18:38 +0300

    Updates

--- test/test_markets.py
+++ test/test_markets.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
 import path_config #Nu uita sa modifici path-ul in fisierul path_config.py cu folderul proiectului tau
 import unittest
-from page.about_page import MarketsPage
+from page.markets_page import MarketsPage
 
 
 class ProductTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
 
-    def test_aboutNav(self):
-        aboutPage = MarketsPage(self.driver)
-        aboutPage.goToMarkets()
-        aboutPage.verifyNavigation()
+    def test_marketsNav(self):
+        marketsPage = MarketsPage(self.driver)
+        marketsPage.goToMarkets()
+        marketsPage.verifyNavigation()
 
     def test_title(self):
-        aboutPage = MarketsPage(self.driver)
-        aboutPage.goToMarkets()
-        aboutPage.verifyTitle()
+        marketsPage = MarketsPage(self.driver)
+        marketsPage.goToMarkets()
+        marketsPage.verifyTitle()
 
     def tearDown(self):
         self.driver.quit()
